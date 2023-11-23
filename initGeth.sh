a64bafcf575d08f33e0a6ee11f85c44f4d815371
wangyufsn <dev.wangyu@proton.me>
2023-11-23 19:00:04 +0800

    up

---
+++ initGeth.sh
@@ -0,0 +1,14 @@
+
+
+docker run \
+  --rm \
+  -it \
+  -v $(pwd)/execution-data:/execution-data \
+  -v $(pwd)/config-example:/config \
+  -v $(pwd)/el-cl-genesis-data:/el-cl-genesis-data \
+  ethereum/client-go:v1.13.4 \
+  --state.scheme=path \
+  --datadir=/execution-data \
+  init \
+  /el-cl-genesis-data/custom_config_data/genesis.json
+
